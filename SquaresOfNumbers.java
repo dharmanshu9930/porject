f20ac7229bf3b33a1eb2540aad2f2ac616370ec7
Amaan072003 <114463474+Amaan072003@users.noreply.github.com>
2022-10-06 11:21:31 +0530

    Create SquaresOfNumbers.java

---
+++ SquaresOfNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class square3 {
+    public static void main(String[] args){
+        Scanner a= new Scanner(System.in);
+        System.out.print
+        ("N : ");
+        int n= a.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.print(i*i+" ");
+        }
+    }
+}
