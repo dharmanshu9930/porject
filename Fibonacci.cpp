0a8623ff992c036082ccd63a563efffa2e690653
shahneha1112 <115452678+shahneha1112@users.noreply.github.com>
2022-10-10 17:31:26 +0530

    Create Fibonacci.cpp

---
+++ Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int fib(int n)
+{
+    int a = 0;
+    int b = 1;
+    int c;
+    int i;
+    if( n == 0)
+        return a;
+    for(i = 2; i <= n; i++)
+    {
+       c = a + b;
+       a = b;
+       b = c;
+    }
+    return b;
+}
+
+int main()
+{
+    int n = 9;
+     
+    cout << fib(n);
+    return 0;
+}
