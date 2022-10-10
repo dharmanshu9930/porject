4b57d21e59f2e97898f4145f256887ad9d6a470f
Dipalikamboj <93436913+Dipalikamboj@users.noreply.github.com>
2022-10-10 17:57:53 +0530

    Hacktoberfest2022

---
+++ Hacktoberfest2022.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int a ;
+
+cin>>a;
+
+if(a%2 == 0)  // if remainder is zero then even number
+
+    cout<<”even”;
+
+else       cout<<”odd”;
+
+return 0;
+
+}
