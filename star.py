4b4fb5b268e0b4cfb2a3b07bf30989260c5c5159
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2022-10-25 19:05:34 +0530

    star.py

---
+++ star.py
@@ -0,0 +1,8 @@
+# Program to print full pyramid 
+num_rows = int(input("Enter the number of rows"));
+for i in range(0, num_rows):
+	for j in range(0, num_rows-i-1):
+		print(end=" ")
+	for j in  range(0, i+1):
+		print("*", end=" ")
+	print()
